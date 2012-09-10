11384e0f32278ebc9c4c9858a760bf8afd564f65
Joe Wood <joewood@live.com>
2012-09-10 16:47:47 -0400

    Relative Paths

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,11 +1,11 @@
 define.amd.jQuery = true
-@require.config
+require.config
     baseUrl: '/scripts',
     paths: 
-        "jquery": "./jquery-1.8.0",
-        "knockout": "./knockout-2.1.0",
-        "jquery.easing" : "./jquery.easing.1.3"
-@require ["jquery","knockout", "demo-viewmodel", "../src/switch-view.js", "../src/external-ko-template.js"], ($,ko,ViewModel,SwitchView) ->
+        "jquery": "jquery-1.8.0",
+        "knockout": "knockout-2.1.0",
+        "jquery.easing" : "jquery.easing.1.3"
+require ["jquery","knockout", "demo-viewmodel", "../src/switch-view.js", "../src/external-ko-template.js"], ($,ko,ViewModel,SwitchView) ->
     $( () ->
         $.ajaxSetup({ cache: false })
         $("#main").text("KO: " + typeof(ViewModel))
--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,16 @@
 
   define.amd.jQuery = true;
 
-  this.require.config({
+  require.config({
     baseUrl: '/scripts',
     paths: {
-      "jquery": "./jquery-1.8.0",
-      "knockout": "./knockout-2.1.0",
-      "jquery.easing": "./jquery.easing.1.3"
+      "jquery": "jquery-1.8.0",
+      "knockout": "knockout-2.1.0",
+      "jquery.easing": "jquery.easing.1.3"
     }
   });
 
-  this.require(["jquery", "knockout", "demo-viewmodel", "../src/switch-view.js", "../src/external-ko-template.js"], function($, ko, ViewModel, SwitchView) {
+  require(["jquery", "knockout", "demo-viewmodel", "../src/switch-view.js", "../src/external-ko-template.js"], function($, ko, ViewModel, SwitchView) {
     return $(function() {
       $.ajaxSetup({
         cache: false
